256446a636b05534057a475cd782738f2ead4db1
Karrar Qasim <Karawr1996@gmail.com>
2022-07-12 17:34:37 -0600

    console stat

--- variables.js
+++ variables.js
@@ -1,4 +1,7 @@
 var name = "Karrarkanaanqasimdesher";
+console.log(Thats my real name but not all of it ;);
 var city = "thornton80602";
 var age = 12;
 var age12 = True;
+name = "JustKarrar";
+console.log(This is fun);
